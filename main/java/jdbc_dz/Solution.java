51f15492cd3ff27dd3f81178dae151552ec9f4aa
YuriiS-68 <yurii_s@bigmir.net>
2018-03-09 20:29:48 +0200

    add second commit

--- main/java/jdbc_dz/Solution.java
+++ main/java/jdbc_dz/Solution.java
@@ -1,6 +1,7 @@
 package jdbc_dz;
 
 import java.sql.*;
+import java.util.Arrays;
 
 public class Solution {
     private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lesson.cjqbbseqr63c.eu-central-1.rds.amazonaws.com:1521:ORCL";
@@ -53,19 +54,60 @@ public class Solution {
     private static void getProductsByDescription(){
         try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
 
-            try(ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT")){
+            try(ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT DESCRIPTION WHERE LENGTH(DESCRIPTION) > 50")){
                 while (resultSet.next()){
                     long id = resultSet.getLong(1);
                     String name = resultSet.getString(2);
                     String description = resultSet.getString(3);
                     int price = resultSet.getInt(4);
-                    if (description.length() > 50){
-                        Product product = new Product(id, name, description, price);
-                        System.out.println(product);
-                    }
+                    Product product = new Product(id, name, description, price);
+                    System.out.println(product);
                 }
             }
 
+        }catch (SQLException e){
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+        }
+    }
+
+    private static void increasePrice(){
+        try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement()) {
+
+            int response = statement.executeUpdate("UPDATE PRODUCT SET PRICE = PRICE + 100 WHERE PRICE < 970");
+
+        }catch (SQLException e){
+            System.err.println("Something went wrong");
+            e.printStackTrace();
+        }
+    }
+
+    private static void changeDescription(){
+        String sql = "UPDATE PRODUCT SET DESCRIPTION = ? WHERE LENGTH(DESCRIPTION) > 40 AND ID = ?";
+
+        try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASS); Statement statement = connection.createStatement(); PreparedStatement ps = connection.prepareStatement(sql)) {
+
+            try(ResultSet resultSet = statement.executeQuery("SELECT * FROM PRODUCT DESCRIPTION WHERE LENGTH(DESCRIPTION) > 40")){
+                while (resultSet.next()){
+                    String description = resultSet.getString(3);
+                    int idRow = resultSet.getInt(1);
+
+                    String[] arraySentences = description.split("[.]");
+
+                    arraySentences[arraySentences.length - 1] = "";
+
+                    String descriptionNew = "";
+                    for (String sentence : arraySentences){
+                        if (sentence != null && !sentence.isEmpty()){
+                            descriptionNew += sentence + ".";
+                        }
+                    }
+
+                    ps.setString(1, descriptionNew);
+                    ps.setInt(2, idRow);
+                    ps.executeUpdate();
+                }
+            }
         }catch (SQLException e){
             System.err.println("Something went wrong");
             e.printStackTrace();
@@ -103,6 +145,12 @@ public class Solution {
 
         //getProductsByPrice();
 
-        getProductsByDescription();
+        //getProductsByDescription();
+
+        //increasePrice();
+
+        changeDescription();
+
+        //changeSentence("railroad with locomotive and wagons. this toy is like every child. she brings joy.");
     }
 }
