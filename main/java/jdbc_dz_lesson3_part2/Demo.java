1e20a4f0f815a1bf98d26724be07f9edd42394a5
YuriiS-68 <yurii_s@bigmir.net>
2018-03-12 20:33:57 +0200

    add lesson3 part2

---
+++ main/java/jdbc_dz_lesson3_part2/Demo.java
@@ -0,0 +1,16 @@
+package jdbc_dz_lesson3_part2;
+
+public class Demo {
+    public static void main(String[] args)throws Exception {
+
+        ProductDAO productDAO = new ProductDAO();
+
+        //System.out.println(productDAO.findProductsByPrice(200, 50));
+
+        //Product product = new Product(1122, "test-1", "test description 1", 200);
+
+        //System.out.println(productDAO.findProductsByName("plate"));
+
+        System.out.println(productDAO.findProductsWithEmptyDescription());
+    }
+}
