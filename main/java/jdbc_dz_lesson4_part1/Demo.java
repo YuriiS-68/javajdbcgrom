ad785134098ffbdd0d714211ba6ec6e7004bcd56
YuriiS-68 <yurii_s@bigmir.net>
2018-03-22 20:10:24 +0200

    add new home work lesson4 part1

---
+++ main/java/jdbc_dz_lesson4_part1/Demo.java
@@ -0,0 +1,22 @@
+package jdbc_dz_lesson4_part1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static jdbc_dz_lesson4_part1.TransactionDemo.save;
+
+public class Demo {
+    public static void main(String[] args){
+        Product product1 = new Product(55, "!!!", "!!!", 777);
+        Product product2 = new Product(66, "!!!", "!!!", 777);
+        Product product3 = new Product(66, "!!!", "!!!", 777);
+
+        List<Product> products = new ArrayList<>();
+        products.add(product1);
+        products.add(product2);
+        products.add(product3);
+
+        save(products);
+    }
+
+}
--- main/java/jdbc_dz_lesson4_part1/TransactionDemo.java
+++ main/java/jdbc_dz_lesson4_part1/TransactionDemo.java
@@ -1,6 +1,4 @@
-package lesson4;
-
-import lesson3.Product;
+package jdbc_dz_lesson4_part1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -10,24 +8,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TransactionDemo {
-    //1. save order - pay money - receive money + ...
-    //2. save order - pay money - receive money
+
     private static final String DB_URL = "jdbc:oracle:thin:@gromcode-lesson.cjqbbseqr63c.eu-central-1.rds.amazonaws.com:1521:ORCL";
     private static final String USER = "main";
     private static final String PASS = "ifgjrkzr";
-
-    public static void main(String[] args){
-        Product product1 = new Product(55, "!!!", "!!!", 777);
-        Product product2 = new Product(66, "!!!", "!!!", 777);
-        Product product3 = new Product(66, "!!!", "!!!", 777);
-
-        List<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
-        products.add(product3);
-
-        save(products);
-    }
 
     public static void save(List<Product> products){
         try(Connection connection = getConnection()) {
@@ -41,11 +25,13 @@ public class TransactionDemo {
     }
 
     private static void saveList(List<Product> products, Connection connection)throws SQLException{
+        long badId = 0;
         try(PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO PRODUCT VALUES(?, ?, ?, ?)")) {
 
             connection.setAutoCommit(false);
 
             for (Product product : products){
+                badId = product.getId();
                 preparedStatement.setLong(1, product.getId());
                 preparedStatement.setString(2, product.getName());
                 preparedStatement.setString(3, product.getDescription());
@@ -60,6 +46,7 @@ public class TransactionDemo {
 
         }catch (SQLException e){
             connection.rollback();
+            System.err.println("Product with id - " + badId + " failed to save in DB.");
             throw e;
         }
     }
