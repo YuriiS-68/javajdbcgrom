0c9844ced30e452a236dff1f9ed94e8ec6ae2a30
YuriiS-68 <yurii_s@bigmir.net>
2018-04-25 18:29:44 +0300

    added first dz by hibernate

---
+++ main/java/jdbc_dz_lesson5_part1/HibernateUtils.java
@@ -0,0 +1,13 @@
+package jdbc_dz_lesson5_part1;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtils {
+
+    private SessionFactory sessionFactory;
+
+    public SessionFactory createSessionFactory(){
+        return new Configuration().configure().buildSessionFactory();
+    }
+}
---
+++ main/java/jdbc_dz_lesson5_part1/ProductRepository.java
@@ -0,0 +1,39 @@
+package jdbc_dz_lesson5_part1;
+
+import lesson5.HibernateUtils;
+import org.hibernate.Session;
+
+public class ProductRepository {
+
+    private Session session = new HibernateUtils().createSessionFactory().openSession();
+
+    void save(Product product){
+        session.getTransaction().begin();
+
+        session.save(product);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
+    void delete(Product product){
+        session.getTransaction().begin();
+
+        session.delete(product);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+
+    void update(Product product){
+        session.getTransaction().begin();
+
+        session.update(product);
+
+        session.getTransaction().commit();
+
+        session.close();
+    }
+}
